c68ce0c78ee8dadec10c6867228e48b48085ef2b
nandannegi <neginandan37@gmail.com>
2020-03-13 18:22:06 +0530

    add file

---
+++ js/login-popUp.js
@@ -0,0 +1,15 @@
+var x =document.getElementById("login");
+  var y =document.getElementById("register");
+  var z =document.getElementById("btn");
+
+  function login(){
+      x.style.left = "0px"
+      y.style.left = "550px"
+      z.style.left = "0px"
+  } 
+  
+  function register(){
+      x.style.left = "-550px"
+      y.style.left = "0px"
+      z.style.left = "115px"
+  }
